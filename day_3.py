c243d2ef83134f210ca37920e4c7c9136b8eb6b4
Peerapat Luxsuwong <pluxsuwong@wpi.edu>
2015-12-22 05:22:43 -0500

    day_3 2 star

--- day_3.py
+++ day_3.py
@@ -1,21 +1,38 @@
 f = open('day_3', 'r')
 d = f.read()
+turn = 0
 houses = []
-x = 0
-y = 0
-h_id = str(x) + ',' + str(y)
+x_0 = 0
+y_0 = 0
+x_1 = 0
+y_1 = 0
+h_id = str(x_0) + ',' + str(y_0)
 houses.append(h_id)
 for i in d:
-    if i == '>':
-        x += 1
-    elif i == '<':
-        x -= 1
-    elif i == '^':
-        y += 1
-    elif i == 'v':
-        y -= 1
-    h_id = str(x) + ',' + str(y)
-    houses.append(h_id)
+    if turn == 0:
+        if i == '>':
+            x_0 += 1
+        elif i == '<':
+            x_0 -= 1
+        elif i == '^':
+            y_0 += 1
+        elif i == 'v':
+            y_0 -= 1
+        h_id = str(x_0) + ',' + str(y_0)
+        houses.append(h_id)
+        turn = 1
+    else:
+        if i == '>':
+            x_1 += 1
+        elif i == '<':
+            x_1 -= 1
+        elif i == '^':
+            y_1 += 1
+        elif i == 'v':
+            y_1 -= 1
+        h_id = str(x_1) + ',' + str(y_1)
+        houses.append(h_id)
+        turn = 0
 
 print len(set(houses))
 
