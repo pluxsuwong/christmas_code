2c0dbc0ec842273d7024447c5169fe1d1ca8f76c
Peerapat Luxsuwong <pluxsuwong@wpi.edu>
2015-12-23 08:48:42 -0500

    day_13 1 star

--- day_13.py
+++ day_13.py
@@ -1,6 +1,50 @@
+import copy
+
+def max_path(first_node, last_node, rem_nodes):
+    if len(rem_nodes) == 1:
+        e_costs_f = graph[first_node]
+        e_costs_l = graph[last_node]
+        e_costs_r = graph[rem_nodes[0]]
+        cost_buf = 0
+        for tup_f in e_costs_f:
+            if tup_f[0] == rem_nodes[0]:
+                cost_buf += tup_f[1]
+                break
+        for tup_l in e_costs_l:
+            if tup_l[0] == rem_nodes[0]:
+                cost_buf += tup_l[1]
+                break
+        for tup_r in e_costs_r:
+            if tup_r[0] == first_node or tup_r[0] == last_node:
+                cost_buf += tup_r[1]
+        return cost_buf
+
+    max_c = 0
+    for n in rem_nodes:
+        node_cp = copy.deepcopy(rem_nodes)
+        node_cp.remove(n)
+        cur_cost = 0
+        e_costs_l = graph[last_node]
+        e_costs_r = graph[n]
+        for tup_l in e_costs_l:
+            if tup_l[0] == n:
+                cur_cost += tup_l[1]
+                break
+        for tup_r in e_costs_r:
+            if tup_r[0] == last_node:
+                cur_cost += tup_r[1]
+                break
+        path_cost = cur_cost + max_path(first_node, n, node_cp)
+        if path_cost > max_c:
+            max_c = path_cost
+    return max_c
+
+
 f = open('day_13', 'r')
 
-fb = {}
+global graph, seating
+graph = {}
+seating = {}
 
 s = f.readline().rstrip()
 while s != '':
@@ -13,11 +57,19 @@ while s != '':
     else:
         eff = int(e[3])
     try:
-        fb[p_0].append((p_1, eff))
+        graph[p_0].append((p_1, eff))
     except:
-        fb[p_0] = [(p_1, eff)]
+        graph[p_0] = [(p_1, eff)]
     s = f.readline().rstrip()
 
+nodes = graph.keys()
 
+max_cost = 0
+for n in nodes:
+    node_cp = copy.deepcopy(nodes)
+    node_cp.remove(n)
+    path_cost = max_path(n, n, node_cp)
+    if path_cost > max_cost:
+        max_cost = path_cost
 
-print fb
+print max_cost
