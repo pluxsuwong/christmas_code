f6ad4fae14993756e7087efb6beb5a4408e480f1
Peerapat Luxsuwong <pluxsuwong@wpi.edu>
2015-12-23 16:31:36 -0500

    day_17 pending 1 star

---
+++ day_17.py
@@ -0,0 +1,54 @@
+import copy
+
+def cur_vol(containers):
+    tally = 0
+    for tup in containers:
+        tally += tup[1]
+    return tally
+
+def progress(cur_containers, rem_containers):
+    if cur_vol(cur_containers) == 150:
+        return cur_containers
+    elif cur_vol(cur_containers) > 150:
+        return None
+
+    combos = set([])
+    while rem_containers:
+        c = rem_containers.pop(0)
+        containers_cp = copy.deepcopy(rem_containers)
+        new_cur_containers = copy.deepcopy(cur_containers)
+        new_cur_containers.append(c)
+        more_combos = progress(new_cur_containers, containers_cp)
+        if more_combos != None:
+            for combo in more_combos:
+                combos.add(combo)
+    return combos
+
+f = open('day_17', 'r')
+
+unsorted_containers = []
+num_id = 0
+s = f.readline().rstrip()
+while s != '':
+    unsorted_containers.append((num_id, int(s)))
+    num_id += 1
+    s = f.readline().rstrip()
+
+containers = []
+while unsorted_containers:
+    max_c = unsorted_containers[0]
+    for u_c in unsorted_containers:
+        if u_c[1] > max_c[1]:
+            max_c = u_c
+    containers.append(max_c)
+    unsorted_containers.remove(max_c)
+
+combos = set([])
+while containers:
+    c = containers.pop(0)
+    containers_cp = copy.deepcopy(containers)
+    more_combos = progress([c], containers_cp)
+    for combo in more_combos:
+        combos.add(combo)
+
+print len(combos)
